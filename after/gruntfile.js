d06920f20ac12062a6ccbbae39d41d89abd5e81a
Dean <deanhume@gmail.com>
2015-09-02 14:34:56 +0100

    Updated packages

--- after/gruntfile.js
+++ after/gruntfile.js
@@ -8,11 +8,24 @@ module.exports = function(grunt) {
       { src: ['css/material-design.css', 'css/site.css'], dest: 'css/result.min.css' }
       ]
     }
+  },
+  // Image minification
+  imagemin: {                          
+    dynamic: {                        
+      files: [{
+        expand: true,                 
+        cwd: 'images/',                  
+        src: ['**/*.{png,jpg,gif}'],   
+        dest: 'images/dist/'                  
+      }]
+    }
   }
+
 });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['cssmin']);
+  grunt.registerTask('default', ['cssmin', 'imagemin']);
 
 };
