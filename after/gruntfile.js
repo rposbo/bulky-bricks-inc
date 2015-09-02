478472ad1ddaa33a8e4a782660ea120c4f727ff9
Dean <deanhume@gmail.com>
2015-09-02 15:08:52 +0100

    Added svgmin

--- after/gruntfile.js
+++ after/gruntfile.js
@@ -10,22 +10,40 @@ module.exports = function(grunt) {
     }
   },
   // Image minification
-  imagemin: {                          
-    dynamic: {                        
-      files: [{
-        expand: true,                 
-        cwd: 'images/',                  
-        src: ['**/*.{png,jpg,gif}'],   
-        dest: 'images/dist/'                  
-      }]
+  // imagemin: {                          
+  //   dynamic: {                        
+  //     files: [{
+  //       expand: true,                 
+  //       cwd: 'images/',                  
+  //       src: ['**/*.{png,jpg,gif}'],   
+  //       dest: 'images/dist/'                  
+  //     }]
+  //   }
+  // },
+  // Minify SVG
+  svgmin: {
+        options: {
+            plugins: [
+                {
+                    removeViewBox: true
+                }, {
+                    removeUselessStrokeAndFill: true
+                }
+            ]
+        },
+        dist: {
+            files: {
+                'images/dist/logo.svg': 'images/logo.svg'
+            }
+        }
     }
-  }
 
 });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
 
-  grunt.registerTask('default', ['cssmin', 'imagemin']);
+  grunt.registerTask('default', ['cssmin', 'svgmin']);
 
 };
