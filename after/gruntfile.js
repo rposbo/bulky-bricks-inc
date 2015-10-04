de16c93a98e0234a728dffedc61a188fcfd9fc79
Dean <contact@deanhume.com>
2015-10-04 14:20:43 +0100

    Updated grunt file to include page speed

--- after/gruntfile.js
+++ after/gruntfile.js
@@ -1,57 +1,63 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-   // Minify the CSS
-   cssmin: {
-   	target: {
-   		files: [
-   		{ src: ['css/material-design.css', 'css/site.css'], dest: 'css/result.min.css' }
-   		]
-   	}
-   },
-  // Image minification
-  // imagemin: {                          
-  //   dynamic: {                        
-  //     files: [{
-  //       expand: true,                 
-  //       cwd: 'images/',                  
-  //       src: ['**/*.{png,jpg,gif}'],   
-  //       dest: 'images/dist/'                  
-  //     }]
-  //   }
-  // },
-  // Minify SVG
-  svgmin: {
-  	options: {
-  		plugins: [
-  		{
-  			removeViewBox: true
-  		}, {
-  			removeUselessStrokeAndFill: true
-  		}
-  		]
-  	},
-  	dist: {
-  		files: {
-  			'images/dist/logo.svg': 'images/logo.svg'
-  		}
-  	}
-  },
-  perfbudget: {
-  	default: {
-  		options: {
-  			url: 'http://google.com',
-  			key: 'AIzaSyDA3hMmqJaXRcnnZ83vRBV7K5eLTUWwuoA'
-  		}
-  	}
-  }
-});
+		// Minify the CSS
+		cssmin: {
+			target: {
+				files: [
+					{ src: ['css/material-design.css', 'css/site.css'], dest: 'css/result.min.css' }
+				]
+			}
+		},
+		// Image minification
+		// imagemin: {
+		//   dynamic: {
+		//     files: [{
+		//       expand: true,
+		//       cwd: 'images/',
+		//       src: ['**/*.{png,jpg,gif}'],
+		//       dest: 'images/dist/'
+		//     }]
+		//   }
+		// },
+		// Minify SVG
+		svgmin: {
+			options: {
+				plugins: [
+					{
+						removeViewBox: true
+					}, {
+						removeUselessStrokeAndFill: true
+					}
+				]
+			},
+			dist: {
+				files: {
+					'images/dist/logo.svg': 'images/logo.svg'
+				}
+			}
+		},
+		pagespeed: {
+			options: {
+				nokey: true,
+				url: "http://rposbo.github.io"
+			},
+			prod: {
+				options: {
+					url: "http://rposbo.github.io/bulky-bricks-inc/after/index.html",
+					locale: "en_GB",
+					strategy: "desktop",
+					threshold: 80
+				}
+			}
+		}
+	});
 
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	//grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-svgmin');
-	grunt.loadNpmTasks('grunt-perfbudget');
+	grunt.loadNpmTasks('grunt-pagespeed');
 
-	grunt.registerTask('default', ['cssmin', 'svgmin', 'perfbudget']);
+	grunt.registerTask('default', ['pagespeed', 'cssmin', 'svgmin']);
 
 };
