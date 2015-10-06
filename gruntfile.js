712c4a4e63157b49cdafeb430400c0456c350202
Dean <deanhume@gmail.com>
2015-10-06 11:30:58 +0100

    Removed un-css from gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,42 +1,42 @@
 var timer = require("grunt-timer");
 
 module.exports = function(grunt) {
-timer.init(grunt);
+	timer.init(grunt);
 
-var imageminMozjpeg = require('imagemin-mozjpeg');
+	var imageminMozjpeg = require('imagemin-mozjpeg');
 
 	grunt.initConfig({
 		// Minify the CSS
 		cssmin: {
 			target: {
 				files: [
-					{ src: ['after/css/material-design.css', 'after/css/site.css'], dest: 'after/css/result.min.css' }
+				{ src: ['after/css/material-design.css', 'after/css/site.css'], dest: 'after/css/result.min.css' }
 				]
 			}
 		},
 		// Image minification
-	   imagemin: {                          
-	     dynamic: {                                        
-	         options: {
-	           use: [imageminMozjpeg( {quality:80, quantTable: 3} )]
-	         },         
-	       files: [{
-	         expand: true,                 
-	         cwd: 'after/images/',                  
-	         src: ['*.{png,jpg,gif}'],   
-	         dest: 'after/images/dist/'                  
-	       }]
-	     }
-	   },
+		imagemin: {                          
+			dynamic: {                                        
+				options: {
+					use: [imageminMozjpeg( {quality:80, quantTable: 3} )]
+				},         
+				files: [{
+					expand: true,                 
+					cwd: 'after/images/',                  
+					src: ['*.{png,jpg,gif}'],   
+					dest: 'after/images/dist/'                  
+				}]
+			}
+		},
 		// Minify SVG
 		svgmin: {
 			options: {
 				plugins: [
-					{
-						removeViewBox: true
-					}, {
-						removeUselessStrokeAndFill: true
-					}
+				{
+					removeViewBox: true
+				}, {
+					removeUselessStrokeAndFill: true
+				}
 				]
 			},
 			dist: {
@@ -61,73 +61,51 @@ var imageminMozjpeg = require('imagemin-mozjpeg');
 		},
 		// Minify the JavaScript
 		uglify: {
-		  dist: {
-		    files: [{
-		      expand: true,
-		      cwd: 'after/scripts',
-		      src: ['*.js', '!*.min.js'],
-		      dest: 'after/scripts',
-		      ext: '.min.js'
-		    }]
-		  }
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'after/scripts',
+					src: ['*.js', '!*.min.js'],
+					dest: 'after/scripts',
+					ext: '.min.js'
+				}]
+			}
 		},
-		// Removed any unused CSS
-		uncss: {
-		  dist: {
-		    files: {
-		      'after/css/index.css': ['after/index.html']
-		    }
-		  }
-		},
-		// Rewrite any CSS links to the new clean CSS
-		 processhtml: {
-		    options: {
-		      data: {
-		        //message: 'Hello world!'
-		      }
-		    },
-		    dist: {
-		      files: {
-		        'after/processed-index.html': ['after/index.html']
-		      }
-		    }
-		  },
 		// Minify the HTML
 		htmlmin: {                                   
-		    dist: {                                      
-		        options: {                                 
-		            removeComments: true,
-		            collapseWhitespace: true,
-		            conservativeCollapse: true,
-		            collapseBooleanAttributes: true,
-		            removeAttributeQuotes: true,
-		            removeRedundantAttributes: true,
-		            keepClosingSlash: true,
-		            minifyJS: true,
-		            minifyCSS: true
-		        },
-		        files: [
-		            {
-		                expand: true,
-		                cwd: 'after/',
-		                src: ['*.html'],
-		                dest: 'after/html/',
-		                ext: '.html'
-		            }
-		        ]
-		    }
+			dist: {                                      
+				options: {                                 
+					removeComments: true,
+					collapseWhitespace: true,
+					conservativeCollapse: true,
+					collapseBooleanAttributes: true,
+					removeAttributeQuotes: true,
+					removeRedundantAttributes: true,
+					keepClosingSlash: true,
+					minifyJS: true,
+					minifyCSS: true
+				},
+				files: [
+				{
+					expand: true,
+					cwd: 'after/',
+					src: ['*.html'],
+					dest: 'after/html/',
+					ext: '.html'
+				}
+				]
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-svgmin');
-	grunt.loadNpmTasks('grunt-pagespeed');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-uncss');
-	grunt.loadNpmTasks('grunt-contrib-htmlmin');
-	grunt.loadNpmTasks('grunt-processhtml');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-svgmin');
+grunt.loadNpmTasks('grunt-pagespeed');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-htmlmin');
+grunt.loadNpmTasks('grunt-processhtml');
 
-	grunt.registerTask('default', ['pagespeed', 'imagemin', 'uglify', 'cssmin', 'svgmin', 'uncss', 'processhtml', 'htmlmin']);
+grunt.registerTask('default', ['pagespeed', 'imagemin', 'uglify', 'cssmin', 'svgmin', 'processhtml', 'htmlmin']);
 };
 
